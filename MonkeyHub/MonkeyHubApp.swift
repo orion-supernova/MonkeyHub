2c6005873b62460e2dacf60794973a646ae3043c
Murat Can Koç <muratcankoc@gmail.com>
2022-01-16 21:42:13 +0300

    Push notification template added

--- MonkeyHub/MonkeyHubApp.swift
+++ MonkeyHub/MonkeyHubApp.swift
@@ -7,13 +7,40 @@
 
 import SwiftUI
 import Firebase
+import FirebaseMessaging
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+
+        // APNS Registration
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in
+            guard success else { return }
+        }
+        application.registerForRemoteNotifications()
+
+        return true
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        //
+    }
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        //
+    }
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+            guard let token = token else { return }
+            print("DEBUG: APNS Token: " + token)
+        }
+    }
+}
 
 @main
 struct MonkeyHubApp: App {
-
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
